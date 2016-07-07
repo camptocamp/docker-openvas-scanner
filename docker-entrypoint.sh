c473183ed4775132200242d30d0a8739db03945d
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-07-07 09:17:24 +0200

    Add logging

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+echo "Certificate Generation..."
 openvas-mkcert -q
 openvas-mkcert-client -ni
+
+echo "Synchronizing with the OpenVAS NVT Feed..."
 openvas-nvt-sync
 
+echo "Launching openvas-scanner..."
 exec /usr/local/sbin/openvassd $*
